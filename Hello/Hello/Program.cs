ce0fd5c023477bbc2e4c0a56385817dd95d87a76
Suthas Ganeshathasan <suthas.gan@gmail.com>
2019-10-14 11:21:04 -0400

    Module 2
    Module 2 done

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?:");
+            string name = Console.ReadLine();
+            Console.WriteLine("How many hours of sleep did your get last night?");
+            int hoursOfSleep = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, " + name);
+            if (hoursOfSleep >= 8) 
+            {
+                Console.WriteLine("You are well rested");
+            }
+            else
+            {
+                Console.WriteLine("you need more sleep");
+            }
+
+        }
+    }
+}
